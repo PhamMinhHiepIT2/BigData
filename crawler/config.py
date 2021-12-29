665b32f1c10db8d4027057ebf6da8fe49405307f
PhamMinhHiep <hieppm@vingroup.net>
2021-12-29 16:49:08 +0700

    update crawler

---
+++ crawler/config.py
@@ -0,0 +1,6 @@
+HEADER = {
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
+}
+
+URL = "https://tiki.vn/api/v2/products?limit=48&include=advertisement&aggregations=1&category={}&page={}"
+PRODUCT_URL = "https://tiki.vn/api/v2/products/{}"
